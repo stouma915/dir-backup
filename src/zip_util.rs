a374509e1224bd892c0b52321fc892b6fad98bb7
stouma915 <sasakitouma@yahoo.ne.jp>
2022-02-21 21:54:04 +0900

    optionsをconstantにする

--- src/zip_util.rs
+++ src/zip_util.rs
@@ -3,22 +3,27 @@ use std::fs::{DirEntry, File};
 use std::io::{Error, Write};
 
 use colored::Colorize;
+use once_cell::sync::Lazy;
+use zip::write::FileOptions;
 use zip::ZipWriter;
 
 use crate::util;
+
+const OPTIONS: Lazy<FileOptions> = Lazy::new(|| {
+    zip::write::FileOptions::default()
+        .compression_method(zip::CompressionMethod::Stored)
+        .large_file(true)
+});
 
 pub fn write_zip(
     mut writer: ZipWriter<File>,
     entries: Vec<DirEntry>,
 ) -> Result<ZipWriter<File>, String> {
-    let options =
-        zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Stored);
-
     for entry in entries {
         let entry_name = entry.path().into_os_string().into_string().unwrap();
 
         if !entry.metadata().unwrap().is_dir() {
-            match writer.start_file(&entry_name, options) {
+            match writer.start_file(&entry_name, *OPTIONS) {
                 Ok(_) => match util::read_bytes(entry.path()) {
                     Ok(buffer) => match writer.write(&*buffer) {
                         Ok(_) => {
@@ -38,7 +43,7 @@ pub fn write_zip(
                 }
             }
         } else {
-            match writer.add_directory(&entry_name, options) {
+            match writer.add_directory(&entry_name, *OPTIONS) {
                 Ok(_) => match fs::read_dir(&entry_name) {
                     Ok(paths) => {
                         println!("{} {}", "Complete:".bright_blue(), &entry_name);
